e806f287618d1d3e260620c41258534ff2ce3bdf
John Gomez <gomezjohn@tuta.com>
2026-05-10 08:47:37 -0400

    First Commit

---
+++ src/main/java/com/devsu/App.java
@@ -0,0 +1,13 @@
+package com.devsu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
